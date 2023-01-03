9b9933543322736e1f56aba66f7b08b6f17cea64
Tim Logan <tlo@lattec.com>
2023-01-03 11:12:13 +0100

    Adding support for db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as db from './db.json';
 
 @Module({
   imports: [
@@ -14,16 +15,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     //   debug: true,
     //   playground: true,
     // }),
-    // TypeOrmModule.forRoot({
-    //   type: 'mssql',
-    //   host: 'storii.database.windows.net',
-    //   port: 1433,
-    //   username: 'CloudSA7ed77d60',
-    //   password: 'SwordOfDoom+3',
-    //   database: 'StoriiDb',
-    //   entities: [],
-    //   synchronize: true, //TODO: make this depend on environment, dangerous in production
-    // }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [],
+      synchronize: true, //TODO: make this depend on environment, dangerous in production
+    }),
     AuthModule,
     UsersModule,
   ],
@@ -31,4 +28,3 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   providers: [AppService],
 })
 export class AppModule {}
-// Server=tcp:storii.database.windows.net,1433;Initial Catalog=StoriiDb;Persist Security Info=False;User ID=CloudSA7ed77d60;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
